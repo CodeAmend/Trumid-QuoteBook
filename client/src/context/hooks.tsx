2ad43c7859c435dfd7189833c86f22e96dd49516
Michael Bruce Allen <CodeAmend@Gmail.Com>
2020-05-06 00:06:07 -0400

    Add crud quote hooks and types

--- client/src/context/hooks.tsx
+++ client/src/context/hooks.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import quoteBookContext from '.';
-import { IUseQuoteBook, BestBidBuy } from './types';
+import {
+  IUseQuoteBook,
+  BestBidBuy,
+  ReplaceQuote,
+  CreateQuote,
+  CancelQuote,
+} from './types';
 import { reduceBondQuotes, getBestBidsFromReducedBonds } from './utils';
 
 export const useQuotebook = (): IUseQuoteBook => {
@@ -9,10 +15,51 @@ export const useQuotebook = (): IUseQuoteBook => {
     lookupTables,
     quoteBook,
   } = React.useContext(quoteBookContext.context)
-  
+
   const updateQuoteBook = (): void => {
     socket.emit('quoteBook.snapshot');
   }
+
+  const subscribeToQuotes = (): void => {
+    socket.emit('quoteBook.subscribe');
+  }
+
+  const unsubscribeFromQuotes = (): void => {
+    socket.emit('quoteBook.unsubscribe');
+  }
+
+  const createQuote = (request: CreateQuote) => {
+    request =  {
+      requestId: Math.random().toString(36).substr(2, 5),
+      accountId: 0,
+      bondId: lookupTables.bonds[0].id,
+      side: 'B',
+      price: 99.975,
+      qty: 1000000
+    };
+    console.log("Outgoing: quote.create", request);
+    socket.emit('quote.create', request);
+  }
+
+  const replaceQuote = (request: ReplaceQuote) => {
+    request = {
+      requestId: Math.random().toString(36).substr(2, 5),
+      quoteId: 'dsafwr', // currentQuote.id,
+      price: 99.975,
+      qty: 1000000
+    };
+    console.log("Outgoing: quote.replace", request);
+    socket.emit('quote.replace', request);
+  }
+
+  const cancelQuote = () => {
+    const request: CancelQuote = {
+      requestId: Math.random().toString(36).substr(2, 5),
+      quoteId: 'fsfsd', // currentQuote.id
+    };
+    socket.emit('quote.cancel', request);
+  }
+
 
   const bestBidBuy: BestBidBuy[] = React.useMemo(() => {
     
@@ -33,6 +80,11 @@ export const useQuotebook = (): IUseQuoteBook => {
     updateQuoteBook,
     quoteBook,
     bestBidBuy,
+    subscribeToQuotes,
+    unsubscribeFromQuotes,
+    createQuote,
+    replaceQuote,
+    cancelQuote,
   }
 }
 
--- client/src/context/types.ts
+++ client/src/context/types.ts
@@ -8,19 +8,6 @@ export type AccountMaster = {
   name: string;
 }
 
-export type BondQuote = {
-  accountId: number;
-  bondId: string;
-  createdAt: string;
-  id: string;
-  price: number;
-  qty: number;
-  sequence: number
-  side: "S" | "B";
-  updatedAt: string;
-  reqId?: string;
-}
-
 export type QuoteAction = {
   action: "N" | "C" | "C";
   quote: BondQuote;
@@ -30,13 +17,6 @@ export type QuoteAction = {
 export type LookUpTables = {
   bonds: BondMaster[];
   accounts: AccountMaster[];
-}
-
-export interface IUseQuoteBook {
-  lookupTables: LookUpTables;
-  updateQuoteBook: () => void; 
-  quoteBook: BondQuote[],
-  bestBidBuy: BestBidBuy[];
 }
 
 export type BidBuy = {
@@ -55,4 +35,61 @@ export interface BestBidBuy {
   bondName: string;
   bid: BidBuy; 
   offer: BidBuy; 
+}
+
+export type BondQuote = {
+  accountId: number;
+  bondId: string;
+  createdAt: string;
+  id: string;
+  price: number;
+  qty: number;
+  sequence: number
+  side: "S" | "B";
+  updatedAt: string;
+  reqId?: string;
+}
+
+export interface CreateQuote {
+  accountId: number;
+  bondId: string;
+  side: 'B' | 'S';
+  price: number;
+  qty: number;
+  requestId: string;
+}
+
+export interface ReplaceQuote {
+  price: number;
+  qty: number;
+  quoteId: string;
+  requestId: string;
+}
+
+export interface QuoteAccepted {
+  action: 'N' | 'U' | 'C';
+  quote: BondQuote;
+  requestId: string;
+}
+
+export interface QuoteRejected {
+  errors: string[];
+  requestId: string;
+}
+
+export interface CancelQuote {
+  requestId: string;
+  quoteId: string;
+}
+
+export interface IUseQuoteBook {
+  lookupTables: LookUpTables;
+  quoteBook: BondQuote[],
+  bestBidBuy: BestBidBuy[];
+  updateQuoteBook: () => void; 
+  subscribeToQuotes: () => void;
+  unsubscribeFromQuotes: () => void;
+  createQuote: (request: CreateQuote) => void;
+  replaceQuote: (request: ReplaceQuote) => void;
+  cancelQuote: () => void;
 }
