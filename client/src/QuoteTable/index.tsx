419245858421c09589dc6e808832bc292c02ccdc
Michael Bruce Allen <CodeAmend@Gmail.Com>
2020-05-07 02:55:42 -0400

    Add AgGrid and theme

--- client/src/QuoteTable/index.tsx
+++ client/src/QuoteTable/index.tsx
@@ -1,40 +1,45 @@
 import React from "react"
-import { Table } from './styles';
-import { BestBidOffer } from '../context/types';
+import { AgGridReact } from 'ag-grid-react';
+// import { Table } from './styles';
+// import { BestBidOffer } from '../context/types';
 import { useQuotebook } from '../context/hooks';
+import styled from 'styled-components';
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham-dark.css';
+
+const AgWraper = styled.section`
+  width: 1400px;
+  height: 100vh;
+`;
 
 const QuoteTable = () => {
   const { bestBidOffer } = useQuotebook();
 
   if (!bestBidOffer.length) return null;
 
+  const columnDefs = [
+    { headerName: "Bond", field: "bondName" },
+    { headerName: "Client", field: "offer.client" },
+    { headerName: "qty", field: "offer.qty" },
+    { headerName: "Price", field: "offer.price" },
+    { headerName: "Price", field: "bid.price" },
+    { headerName: "qty", field: "bid.price" },
+    { headerName: "Client", field: "bid.client" },
+  ];
+
+  const onGridReady = (params: any) => {
+    params.api.sizeColumnsToFit();
+  }
+
   return(
-    <Table>
-      <thead>
-        <tr>
-          <th>Bond</th>
-          <th>Client</th>
-          <th>Qty</th>
-          <th>Bid</th>
-          <th>Offer</th>
-          <th>Qty</th>
-          <th>Client</th>
-        </tr>
-      </thead>
-      <tbody>
-        {bestBidOffer.map(({ bondName, bid, offer }: BestBidOffer) => (
-          <tr key={bondName}>
-            <td>{bondName}</td>
-            <td>{bid?.client}</td>
-            <td>{bid?.qty}</td>
-            <td>{bid?.price}</td>
-            <td>{offer?.price}</td>
-            <td>{offer?.qty}</td>
-            <td>{offer?.client}</td>
-          </tr>
-        ))}
-      </tbody>
-    </Table>
+    <AgWraper className="ag-theme-balham-dark">
+      <AgGridReact
+          columnDefs={columnDefs}
+          rowData={bestBidOffer}
+          onGridReady={onGridReady}
+      />
+    </AgWraper>
   )
 }
 
