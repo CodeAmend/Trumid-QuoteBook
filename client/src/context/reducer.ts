79dc1b311d9bd6b2fbbbc76eb7683dc3101d96ba
Michael Bruce Allen <CodeAmend@Gmail.Com>
2020-05-06 20:09:05 -0400

    Add better name TODO comments

--- client/src/context/reducer.ts
+++ client/src/context/reducer.ts
@@ -7,6 +7,7 @@ import {
 
 export const quoteBookReducer = (state: any, action: any): any => {
   switch (action.type) {
+    // TODO: DEPTH_OF_BOOK
     case actionTypes.CONVERT_SNAPSHOTS:
       const { accountMaster, bondMaster, quoteBook } = action.payload;
       const bondsByBondId = combineSnapshotsToBondIdKeyValues({
@@ -16,6 +17,7 @@ export const quoteBookReducer = (state: any, action: any): any => {
       })
       return { ...state, bondsByBondId }
 
+    // TODO: TOP_OF_BOOK
     case actionTypes.BEST_BIDS:
       const bestBids = getBestBidsFromBondIdKeyValues(action.payload);
       return { ...state, bestBids }
