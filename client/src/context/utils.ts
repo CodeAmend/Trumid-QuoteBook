f6d01b5ccf22fb189ae9163cdda8f8539373056c
Michael Bruce Allen <CodeAmend@Gmail.Com>
2020-05-07 01:25:02 -0400

    Update getBestBidsFromBondIdKeyValues

--- client/src/context/utils.ts
+++ client/src/context/utils.ts
@@ -3,6 +3,7 @@ import {
   BondQuote,
   ReducerState,
   DepthOfBook,
+  BondsByBids,
 } from './types';
 
 
@@ -70,70 +71,20 @@ const sorter = (sortType: string) => (a: any, b: any) => {
 
 export const byPrice = sorter('price');
 
+export const getBestBidsFromBondIdKeyValues = (depthOfBook: DepthOfBook): BondsByBids[] => {
+  const topBidOffers: BondsByBids[] = [];
+
+  for (let [, { bondName, bids, offers, bondId }] of Object.entries(depthOfBook)) {
+    const bestBid: QuoteFigures = bids.sort(byPrice)[0];
+    const bestOffer: QuoteFigures = offers.sort(byPrice)[0];
+    topBidOffers.push({
+      bondName,
+      bondId,
+      bid: bestBid || null,
+      offer: bestOffer || null,
+    })
+  }
+  return topBidOffers; 
+}
 
 
-// export const getBestBidsFromBondIdKeyValues = (bondIdKeyValues: BondsByBondIdKey): BondsByBids[] => {
-//   const topBidOffers: BondsByBids[] = [];
-
-//   for (let [bondId, { bondName, bid, offer }] of Object.entries(bondIdKeyValues)) {
-//     const bestBid: QuoteFigures = bid.sort(byPrice)[0];
-//     const bestOffer: QuoteFigures = offer.sort(byPrice)[0];
-//     topBidOffers.push({
-//       bondName,
-//       bondId,
-//       bid: bestBid || null,
-//       offer: bestOffer || null,
-//     })
-//   }
-//   return topBidOffers; 
-// }
-
-
-// export const combineSnapshotsToBondIdKeyValues = (props: SnapshotProps): BondsByBondIdKey => {
-//   const { quoteBook, accountMaster, bondMaster } = props;
-
-//   const reducedBondsByBondIdKey = quoteBook.reduce((acc: BondsByBondIdKey, item: BondQuote) => {
-//     const { qty, price, bondId } = item;
-
-//     const matchingAccount = accountMaster.find(account => account.id === item.accountId);
-
-//     // TODO: convert to lookup map object key
-//     const matchingBond = bondMaster.find(bond => bond.id === bondId);
-
-
-//     if (!matchingAccount) {
-//       throw new Error(`No account matching accountId ${item.accountId}`)
-//     }
-
-//     if (!matchingBond) {
-//       throw new Error(`No account matching bondId ${bondId}`)
-//     }
-    
-//     // Chreate new KEY value of bondId and bondName, bid, offer
-//     if (!acc[bondId]) {
-//       acc[bondId] = {
-//         bondName: matchingBond.name,
-//         bid: [],
-//         offer: [],
-//       };
-//     } 
-
-//     const bidBuyItem = {
-//       qty: qtyFormat(qty),
-//       price: priceFormat(price),
-//       client: matchingAccount.name
-//     }
-
-//     if (item.side === 'B') {
-//      acc[bondId].offer.push(bidBuyItem);
-//     }
-
-//     if (item.side === 'S') {
-//      acc[bondId].bid.push(bidBuyItem);
-//     } 
-
-//     return acc;
-//   }, {});
-
-//   return reducedBondsByBondIdKey;
-// }
