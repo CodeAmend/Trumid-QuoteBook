b0f30d5a5a34cd6ceb1a71b5e1af1bb8225edd49
Michael Bruce Allen <CodeAmend@Gmail.Com>
2020-05-08 09:05:22 -0400

    Update form functionality

--- client/src/FormView/index.tsx
+++ client/src/FormView/index.tsx
@@ -48,10 +48,11 @@ function FormView() {
       [target.name]: target.value,
     }));
 
-    if (target.name === 'bond') {
+    if (target.name === 'bondId') {
       setSelectedBond(target.value);
     }
   }
+
 
   const handleAddOrder = (): void => {
     const { bondId, side, accountId, price, qty } = selectState;
@@ -64,34 +65,47 @@ function FormView() {
     });
   }
 
-  const disabled: boolean = !(selectState.price > 0 && selectState.qty > 0);
+  const disabled: boolean = !(selectState.bondId && selectState.price > 0 && selectState.qty > 0);
 
   return(
     <form>
-      <button disabled={disabled} type="button" onClick={handleAddOrder}>Add</button>
-      <select name="bond" value={selectState.bondId} onChange={handleSelect}>
+      
+      {!!selectState.bondId && (
+        <button
+          disabled={disabled}
+          type="button"
+          style={{ marginRight: '1rem' }}
+          onClick={handleAddOrder}
+        >Add</button>
+      )}
+
+      <select name="bondId" value={selectState.bondId} onChange={handleSelect}>
+      {!selectState.bondId && <option>Select a Bond</option>}
         {bondSelectItems.length && bondSelectItems.map(({ value, label }) => (
           <option key={value} value={value}>{label}</option>
         ))}
       </select>
 
-      <select value={selectState.accountId} onChange={handleSelect}>
-        {accountSelectItems.length && accountSelectItems.map(({ value, label }) => (
-          <option key={value} value={value}>{label}</option>
-        ))}
-      </select>
+      {!!selectState.bondId && (
+        <>
+        <select value={selectState.accountId} onChange={handleSelect}>
+          {accountSelectItems.length && accountSelectItems.map(({ value, label }) => (
+            <option key={value} value={value}>{label}</option>
+          ))}
+        </select>
 
-      <select value={selectState.side} onChange={handleSelect}>
-        <option value="B">Bid</option>
-        <option value="S">Offer</option>
-      </select>
-      
-      <label htmlFor="qty">Qty (in millions):</label>
-      <input onChange={handleSelect} value={selectState.qty} type="number" id="qty" name="qty" />
+        <select value={selectState.side} onChange={handleSelect}>
+          <option value="B">Bid</option>
+          <option value="S">Offer</option>
+        </select>
+        
+        <label htmlFor="qty">Qty (in millions):</label>
+        <input onChange={handleSelect} value={selectState.qty} type="number" id="qty" name="qty" />
 
-      <label htmlFor="price">Price: $</label>
-      <input onChange={handleSelect} value={selectState.price} type="number" id="price" name="price" />
-
+        <label htmlFor="price">Price: $</label>
+        <input onChange={handleSelect} value={selectState.price} type="number" id="price" name="price" />
+        </>
+      )}
     </form>
   );
 }
